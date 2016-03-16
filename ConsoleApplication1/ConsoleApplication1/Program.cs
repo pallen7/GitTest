bb583aa16b07438cec7ca138ded89a1d85ce1c0e
pallen7 <paulallen7@msn.com>
2016-03-16 15:17:46 +0000

    first commit

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("First check in");
+            Console.WriteLine("First commit in Dev branch");
         }
     }
 }
